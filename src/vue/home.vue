0e16d49762dc7da88b778195d43c71bcc6593a5a
kasumi <develon@qq.com>
2020-02-11 18:58:38 +0800

    src/vue/home.vue

--- src/vue/home.vue
+++ src/vue/home.vue
@@ -1,9 +1,52 @@
 <template>
-    <div>
+    <div center>
+        <div center>{{ result.count | checkCount }} (刷新状态：{{ index }})</div>
+        <div>
+            <img :src='img'/>
+        </div>
+        <div v-if='result.count'>
+            <div result>当前有 {{ result.availables.length }} 个地区可用</div>
+            <div v-for='e in result.availables' :key='e.i'>
+                <div>{{ e.i+1 }}.  {{ e.loc }}  <a :href='e.href'> 点击购买 </a></div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
+import $ from 'jquery';
+var singleData = {
+    result: {},
+    index: 0,
+    img: './target.png',
+}
+function task() {
+    $.getJSON('/check', e => {
+        singleData.result = e;
+        singleData.index ++ ;
+    });
+    setTimeout(task, 200);
+}
+task();
 export default {
-    
+    data() {
+        return singleData;
+    },
+    filters: {
+        checkCount(count) {
+            if (!count) return '正在从服务器获取数据...';
+            return `服务器端已执行 ${ count } 次监控`;
+        }
+    }
 }
 </script>
+<style scoped>
+[center] {
+    text-align: center;
+}
+img {
+    width: 80%;
+}
+div[result] {
+    padding: 20px;
+}
+</style>
